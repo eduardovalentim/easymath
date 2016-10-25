2c429a414e66baf90cd8f7c1fef0db20f791f52a
Eduardo Valentim <valentim.eduardo@gmail.com>
2016-10-25 15:28:26 -0200

    ssh key correction

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 read -p "Deploy to maven cetral repository  (yes/no)? "
 
 if ( [ "$REPLY" == "yes" ] ) then
-  ssh-add ~/.ssh/easymath.pub
+  ssh-add ~/.ssh/easymath
   ssh-add -l
   mvn release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
   ssh-add -D
