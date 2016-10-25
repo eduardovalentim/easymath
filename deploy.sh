43adcee2a2c57ce633ec2187cfe78672e78edd9f
Eduardo Valentim <valentim.eduardo@gmail.com>
2016-10-25 15:22:13 -0200

    ssh key correction

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# Deploy maven artefact in current directory into Maven central repository 
-# using maven-release-plugin goals
 
-read -p "Really deploy to maven cetral repository  (yes/no)? "
+read -p "Deploy to maven cetral repository  (yes/no)? "
 
 if ( [ "$REPLY" == "yes" ] ) then
-  ssh-add ~/.ssh/lubos.krnac
+  ssh-add ~/.ssh/easymath.pub
   ssh-add -l
   mvn release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
   ssh-add -D
