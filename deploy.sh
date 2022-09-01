31b5a56fefb6838f87f75c4447bb08be23433fc9
Valentim <valentim.eduardo@gmail.com>
2022-08-31 23:06:52 -0300

    Simplifying the deployment

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,19 @@ function help() {
    # Display Help
    echo "Usage: deploy.sh [OPTION]..."
    echo "options:"
-   echo "  -c, --clean    Clean up after a release preparation."
-   echo "  -p, --prepare  Prepare for a release in SCM."
-   echo "  -P, --perform  Perform a release from SCM."
-   echo "  -r, --rollback Rollback a previous release."
-   echo "  -h, --help     Display this help and exit."
+   echo "  -c, --clean        Clean up after a release preparation."
+   echo "  -d, --debug        Produce execution debug output."
+   echo "  -g, --gpg-keyname  Produce execution debug output."
+   echo "  -p, --prepare      Prepare for a release in SCM."
+   echo "  -P, --perform      Perform a release from SCM."
+   echo "  -r, --rollback     Rollback a previous release."
+   echo "  -h, --help         Display this help and exit."
    echo
    exit 0
 }
 
 ############################################################
-# Main program                                             #
+# Argument parser                                          #
 ############################################################
 
 POSITIONAL_ARGS=()
@@ -28,6 +30,15 @@ while [[ $# -gt 0 ]]; do
     -c|--clean)
       ARG_CLEAN=yes
       shift # past argument
+      ;;
+    -d|--debug)
+      ARG_DEBUG=yes
+      shift # past argument
+      ;;
+    -g|--gpg-keyname)
+      ARG_GPG_KEYNAME="$2"
+      shift # past argument
+      shift # past value
       ;;
     -p|--prepare)
       ARG_PREPARE=yes
@@ -58,9 +69,31 @@ done
 
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
-# If all arguments are empty
+############################################################
+# Main                                                     #
+############################################################
+
+# If all principal arguments are empty
 if [ -z "$ARG_CLEAN" ] && [ -z "$ARG_PREPARE" ] && [ -z "$ARG_PERFORM" ] && [ -z "$ARG_ROLLBACK" ]; then
+    # Print help and exit
     help
+fi
+
+gpg --list-secret-keys $ARG_GPG_KEYNAME > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    # ---
+    # Just for documentation, the content of the environment variable is generated using this command
+    # ---
+    # gpg --yes --output /tmp/private.pgp --armor --export-secret-key your.username@email &&\
+    # cat -e /tmp/private.pgp | sed 's/\$/\\n/g' | tr --delete '\n' | xsel --clipboard &&\
+    # rm -f /tmp/private.pgp
+    if [ ! -z "$GPG_PRIVATE_KEY" ]; then
+        echo -e $GPG_PRIVATE_KEY | gpg --import
+    else
+        echo "WARNING"
+        echo "WARNING Probably this execution will be aborted with a GPG error due to the lack of the secret key"
+        echo "WARNING"
+    fi
 fi
 
 # If running in a CI server
@@ -68,14 +101,23 @@ if [ ! -z "$SSH_GITHUB_PRIVATE_KEY" ]; then
     mkdir -p ~/.ssh
     chmod 700 ~/.ssh
 
+    # ---
+    # Just for documentation, the content of the environment variable is generated using this command
+    # ---
+    # cat -e ~/.ssh/github.com | sed 's/\$/\\n/g' | tr --delete '\n' | xsel --clipboard
     echo -e $SSH_GITHUB_PRIVATE_KEY > ~/.ssh/github.com
     chmod 600 ~/.ssh/github.com
+else
+    echo "WARNING"
+    echo "WARNING Probably this execution will be aborted with a SSH error due to the lack of the private key"
+    echo "WARNING"
 fi
 
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/github.com
 
-_RUN_MAVEN="mvn --batch-mode --settings .circleci/settings.xml --activate-profiles release --errors"
+# Bake the maven command line
+_RUN_MAVEN="mvn --batch-mode --settings .circleci/settings.xml --activate-profiles release"
 
 if [ ! -z "$ARG_CLEAN" ]; then
     _RUN_MAVEN="$_RUN_MAVEN release:clean"
@@ -91,6 +133,10 @@ fi
 
 if [ ! -z "$ARG_ROLLBACK" ]; then
     _RUN_MAVEN="$_RUN_MAVEN release:rollback"
+fi
+
+if [ ! -z "$ARG_DEBUG" ]; then
+    _RUN_MAVEN="$_RUN_MAVEN --debug"
 fi
 
 eval exec "$_RUN_MAVEN"
